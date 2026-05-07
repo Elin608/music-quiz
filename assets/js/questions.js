e6601451fb27241a3f8a45b7cc1e81cbfad1a4df
Swati0981 <Swati.Gupta@stud.sundsgarden.se>
2026-05-07 10:15:18 +0200

    Add Bollywood quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+const quizQuestions = [
+  {
+    songTitle: "Neele Neele Ambar Par",
+    question: "What is the song title?",
+    options: [
+      "Tum Hi Ho",
+      "Neele Neele Ambar Par",
+      "Jai Ho",
+      "Kal Ho Naa Ho"
+    ],
+    correctAnswer: "Neele Neele Ambar Par"
+  },
+  {
+    songTitle: "Tum Hi Ho",
+    question: "What is the song title?",
+    options: [
+      "Chaiyya Chaiyya",
+      "Tum Hi Ho",
+      "Jai Ho",
+      "Neele Neele Ambar Par"
+    ],
+    correctAnswer: "Tum Hi Ho"
+  },
+  {
+    songTitle: "Jai Ho",
+    question: "What is the song title?",
+    options: [
+      "Kal Ho Naa Ho",
+      "Jai Ho",
+      "Tum Hi Ho",
+      "Chaiyya Chaiyya"
+    ],
+    correctAnswer: "Jai Ho"
+  },
+  {
+    songTitle: "Chaiyya Chaiyya",
+    question: "What is the song title?",
+    options: [
+      "Chaiyya Chaiyya",
+      "Tum Hi Ho",
+      "Jai Ho",
+      "Neele Neele Ambar Par"
+    ],
+    correctAnswer: "Chaiyya Chaiyya"
+  },
+  {
+    songTitle: "Kal Ho Naa Ho",
+    question: "What is the song title?",
+    options: [
+      "Jai Ho",
+      "Kal Ho Naa Ho",
+      "Tum Hi Ho",
+      "Chaiyya Chaiyya"
+    ],
+    correctAnswer: "Kal Ho Naa Ho"
+  }
+];
